6b0c64145a7af0cad5b7158af3c6f27c123b54da
Matteo Saloni <matteo.saloni@gmail.com>
2021-01-21 12:31:07 +0100

    add scala udf wrapper for geohash

---
+++ udf/build.sbt
@@ -0,0 +1,8 @@
+name := "sco-mobilitycovid-udf"
+version := "0.1"
+organization := "it.smartcommunitylab"
+scalaVersion := "2.11.12"
+val sparkVersion = "3.1.0"
+
+libraryDependencies += "org.apache.spark" % "spark-sql_2.12" % "3.0.1"
+libraryDependencies += "com.github.davidmoten" % "geo" % "0.7.7"
---
+++ udf/src/main/scala/it/smartcommunitylab/sco/mobilitycovid/udf/geohash.scala
@@ -0,0 +1,9 @@
+package it.smartcommunitylab.sco.mobilitycovid.udf
+import org.apache.spark.sql.api.java.UDF3
+import com.github.davidmoten.geo._
+
+class geohash extends UDF3[Double, Double, Integer, String] {
+ def call(lat: Double, lon:Double, length:Integer): String = {
+    GeoHash.encodeHash(lat,lon, length)
+ }
+}
